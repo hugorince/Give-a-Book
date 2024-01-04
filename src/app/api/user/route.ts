14ecd2739bc851bedfbd267c3d6bc2a5f128403a
hugorince <hugo.rince@gmail.com>
2024-01-04 21:01:25 +0100

    patch user in progress

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { db } from "@/libs/database";
 import { hash } from "bcrypt";
 import * as z from "zod";
+import { authOptions } from "@/libs/auth/auth";
+import { getServerSession } from "next-auth";
 
 const userSchema = z.object({
   username: z.string().min(1, "Username is required").max(100),
@@ -63,6 +65,25 @@ export const POST = async (req: Request) => {
       { status: 201 },
     );
   } catch (error) {
+    return NextResponse.json(
+      { message: "Something wen wrong" },
+      { status: 500 },
+    );
+  }
+};
+
+export const PATCH = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { email, username, password } = userSchema.parse(body);
+    return NextResponse.json(
+      {
+        user: email,
+        message: "user got successfully",
+      },
+      { status: 201 },
+    );
+  } catch (err) {
     return NextResponse.json(
       { message: "Something wen wrong" },
       { status: 500 },
---
+++ src/app/profile/update-profile/update-profile.tsx
@@ -0,0 +1,5 @@
+import * as z from "zod";
+import { SignUpFormSchema } from "@/libs/types";
+import { useForm } from "react-hook-form";
+
+export const UpdateProfile = () => {};
