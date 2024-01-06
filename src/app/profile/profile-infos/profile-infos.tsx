30ec23d0f103f387e20efee59829a55824d27288
hugorince <hugo.rince@gmail.com>
2024-01-06 20:47:47 +0100

    user modification working

--- src/app/profile/profile-infos/profile-infos.tsx
+++ src/app/profile/profile-infos/profile-infos.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
 import { UpdateProfileInput } from "../update-profile/update-profile-form/update-profile-input/update-profile-input";
 
@@ -8,7 +10,15 @@ export const ProfileInfos = () => {
     label: "",
     placeholder: "",
   });
+
   const { data: session, update } = useSession();
+
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+    },
+  });
 
   const handleOnClick = () => {
     setUpdateInput({
@@ -17,23 +27,72 @@ export const ProfileInfos = () => {
     });
   };
 
+  const handleOnClickEmail = () => {
+    setUpdateInput({
+      label: "email",
+      placeholder: "mail@mail.com",
+    });
+  };
+
+  const handleInputClose = () => {
+    setUpdateInput({
+      label: "",
+      placeholder: "",
+    });
+  };
+
+  const onSubmit = async (values: any) => {
+    const response = await fetch("/api/user", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: session?.user.email,
+        [updateInput.label]: values[`${updateInput.label}`],
+      }),
+    });
+    if (response.ok) {
+      await update({
+        ...session,
+        user: {
+          ...session?.user,
+          [updateInput.label]: values[`${updateInput.label}`],
+        },
+      });
+      console.log("user updated, please re-login to see the effect");
+    } else {
+      console.error("Registration failed");
+    }
+  };
+
   return (
-    <div>
+    <FormProvider {...form}>
       <h1>Welcome {session?.user.username}</h1>
       {updateInput.label === "username" ? (
-        <div>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <UpdateProfileInput type={updateInput} />
-        </div>
+          <button type="submit">update</button>
+          <button onClick={handleInputClose}>cancel</button>
+        </form>
       ) : (
         <div>
           <p>username: {session?.user.username}</p>
           <button onClick={handleOnClick}>update</button>
         </div>
       )}
-      <div>
-        <p>email: {session?.user.email}</p>
-        <button>update</button>
-      </div>
-    </div>
+      {updateInput.label === "email" ? (
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <UpdateProfileInput type={updateInput} />
+          <button>update</button>
+          <button onClick={handleInputClose}>cancel</button>
+        </form>
+      ) : (
+        <div>
+          <p>email: {session?.user.email}</p>
+          <button onClick={handleOnClickEmail}>update</button>
+        </div>
+      )}
+    </FormProvider>
   );
 };
--- src/app/profile/update-profile/update-profile-form/update-profile-input/update-profile-input.tsx
+++ src/app/profile/update-profile/update-profile-form/update-profile-input/update-profile-input.tsx
@@ -1,31 +1,16 @@
 "use client";
 
 import { useFormContext } from "react-hook-form";
-import * as z from "zod";
 
 interface UpdateProfileProps {
   type: {
-    label: "username" | "email" | "password";
+    label: string;
     placeholder: string;
   };
 }
 
-const userSchemaWithId = z.object({
-  username: z.string().min(1, "Username is required").max(100).optional(),
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .email("Invalid email")
-    .optional(),
-  password: z
-    .string()
-    .min(1, "Password is required")
-    .min(8, "Password must have than 8 characters")
-    .optional(),
-});
-
 export const UpdateProfileInput = ({ type }: UpdateProfileProps) => {
-  const form = useFormContext<z.infer<typeof userSchemaWithId>>();
+  const form = useFormContext();
   return (
     <input
       type={type.label}
