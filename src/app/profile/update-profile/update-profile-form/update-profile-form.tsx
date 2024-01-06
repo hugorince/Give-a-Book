c5221702d313efa4c4dac63a2b3566f04fc8d791
hugorince <hugo.rince@gmail.com>
2024-01-06 20:27:21 +0100

    modify logic in progress

--- src/app/profile/update-profile/update-profile-form/update-profile-form.tsx
+++ src/app/profile/update-profile/update-profile-form/update-profile-form.tsx
@@ -2,16 +2,28 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import { useSession } from "next-auth/react";
-import { authOptions } from "@/libs/auth/auth";
+import { UpdateProfileInput } from "./update-profile-input/update-profile-input";
 
 const userSchemaWithId = z.object({
-  username: z.string().min(1, "Username is required").max(100),
-  email: z.string().min(1, "Email is required").email("Invalid email"),
+  username: z.string().min(1, "Username is required").max(100).optional(),
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email("Invalid email")
+    .optional(),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters")
+    .optional(),
 });
 
-export const UpdateProfileForm = () => {
+export const UpdateProfileForm = (type: {
+  label: string;
+  placeholder: string;
+}) => {
   const { data: session, update } = useSession();
   const form = useForm<z.infer<typeof userSchemaWithId>>({
     resolver: zodResolver(userSchemaWithId),
@@ -37,7 +49,7 @@ export const UpdateProfileForm = () => {
         ...session,
         user: {
           ...session?.user,
-          username: values.username,
+          [type.label]: values.type.label,
         },
       });
       console.log("user updated, please re-login to see the effect");
@@ -47,18 +59,13 @@ export const UpdateProfileForm = () => {
   };
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        {...form.register("username")}
-        placeholder="username"
-      />
-      <input
-        type="email"
-        {...form.register("email")}
-        placeholder="mail@mail.com"
-      />
-      <button type="submit">submit</button>
-    </form>
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <UpdateProfileInput
+          type={{ label: "username", placeholder: "username" }}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </FormProvider>
   );
 };
