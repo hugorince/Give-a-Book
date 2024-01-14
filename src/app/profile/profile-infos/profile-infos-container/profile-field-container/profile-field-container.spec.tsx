04f1259c1546f702c4335b9421320c63fe936b43
hugorince <hugo.rince@gmail.com>
2024-01-14 18:41:49 +0100

    fix : wip tests

---
+++ src/app/profile/profile-infos/profile-infos-container/profile-field-container/profile-field-container.spec.tsx
@@ -0,0 +1,38 @@
+import { ProfileFieldContainer } from "./profile-field-container";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+
+const props = {
+  type: "email",
+  handleOnClick: jest.fn(),
+};
+
+jest.mock("next-auth/react");
+
+const mockSession = {
+  data: {
+    user: {
+      email: "test@example.com",
+    },
+  },
+  update: jest.fn(),
+};
+
+useSession.mockReturnValue(mockSession);
+
+describe("ProfileFieldContainer", () => {
+  it("should render the component", () => {
+    render(<ProfileFieldContainer {...props} />);
+    expect(screen.getByText("test@example.com")).toBeInTheDocument();
+  });
+
+  it("should fire the callback when update button is clicked", () => {
+    render(<ProfileFieldContainer {...props} />);
+
+    const updateButton = screen.getByRole("button", { name: "update" });
+    updateButton.click();
+
+    expect(props.handleOnClick).toHaveBeenCalled();
+  });
+});
