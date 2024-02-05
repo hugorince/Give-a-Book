c83102acadf29cf96d981b2c29c4a8bae11c99e9
hugorince <hugo.rince@gmail.com>
2024-02-05 19:45:43 +0100

    test in progress

--- src/app/profile/profile-infos/update-profile-infos-container/update-profile-field-container/update-profile-input/update-profile-input.spec.tsx
+++ src/app/profile/profile-infos/update-profile-infos-container/update-profile-field-container/update-profile-input/update-profile-input.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { UpdateProfileInput } from "./update-profile-input";
 
 jest.mock("react-hook-form", () => ({
@@ -13,6 +13,7 @@ const mockForm = {
 
 describe("UpdateProfileInput", () => {
   beforeEach(() => {
+    jest.clearAllMocks();
     jest
       .requireMock("react-hook-form")
       .useFormContext.mockReturnValue(mockForm);
@@ -23,8 +24,24 @@ describe("UpdateProfileInput", () => {
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
+  it("should call the register function", () => {
+    render(<UpdateProfileInput type="username" />);
+
+    expect(mockForm.register).toHaveBeenCalledWith("username");
+  });
+
   it("should register the value of the input", () => {
     render(<UpdateProfileInput type="username" />);
+
+    const textbox = screen.getByRole("textbox");
+
+    fireEvent.change(textbox, {
+      target: {
+        value: "hugo",
+      },
+    });
+
+    expect(textbox).toHaveValue("hugo");
 
     expect(mockForm.register).toHaveBeenCalledWith("username");
   });
