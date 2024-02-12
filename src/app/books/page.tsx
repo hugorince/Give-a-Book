b32e373e2b9f710c678b9dd32d58641a0dcd8a85
hugorince <hugo.rince@gmail.com>
2024-02-12 19:29:31 +0100

    show images

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -3,13 +3,32 @@ import { db } from "@/libs/database";
 export const Books = async () => {
   const books = await db.book.findMany();
 
+  const displayBooks = await Promise.all(
+    books.map(async (book) => {
+      const userName = await db.user.findUnique({
+        where: { id: book.userId },
+      });
+      return {
+        title: book.title,
+        img: book.image,
+        user: userName?.username,
+      };
+    }),
+  );
+
   return (
     <div>
       <h1>All Books</h1>
       <div>
-        {books.map((book, index) => (
-          <p key={index}>{book.title}</p>
-        ))}
+        {displayBooks.map((book, index) => {
+          return (
+            <div key={index}>
+              <p>{book.title}</p>
+              <img src={book.img || ""} alt="" />
+              <p>user : {book.user}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
