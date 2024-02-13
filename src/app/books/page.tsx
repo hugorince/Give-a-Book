f10cf4d28bd5dc860094bbae0137ad1edb3fed48
hugorince <hugo.rince@gmail.com>
2024-02-13 19:44:32 +0100

    book card working

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,33 +1,16 @@
-import { db } from "@/libs/database";
+import { getBooksData } from "@/libs/utils";
+import { BookCard } from "@/libs/components";
+import classes from "./books.module.css";
 
 export const Books = async () => {
-  const books = await db.book.findMany();
-
-  const displayBooks = await Promise.all(
-    books.map(async (book) => {
-      const userName = await db.user.findUnique({
-        where: { id: book.userId },
-      });
-      return {
-        title: book.title,
-        img: book.image,
-        user: userName?.username,
-      };
-    }),
-  );
+  const displayBooks = await getBooksData();
 
   return (
     <div>
       <h1>All Books</h1>
-      <div>
+      <div className={classes.booksWrapper}>
         {displayBooks.map((book, index) => {
-          return (
-            <div key={index}>
-              <p>{book.title}</p>
-              <img src={book.img || ""} alt="" />
-              <p>user : {book.user}</p>
-            </div>
-          );
+          return <BookCard data={book} key={index} />;
         })}
       </div>
     </div>
--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./fetch-google-api";
 export * from "./post-book";
+export * from "./get-books-data";
