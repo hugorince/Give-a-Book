bc56789d13cf587f820b477adf9c0cfbde7d7c49
hugorince <hugo.rince@gmail.com>
2024-02-16 12:29:57 +0100

    cleaning

--- src/libs/utils/book-utils/book-utils.ts
+++ src/libs/utils/book-utils/book-utils.ts
@@ -1,4 +1,23 @@
+"use server";
+
 import { db } from "@/libs/database";
+import { PostBookFormSchema } from "@/libs/types";
+import { z } from "zod";
+
+export const postBook = async (
+  values: z.infer<typeof PostBookFormSchema>,
+  userId: number,
+) => {
+  await db.book.create({
+    data: {
+      title: values.title,
+      author: values.author,
+      description: values.description,
+      image: values.image,
+      userId: userId as number,
+    },
+  });
+};
 
 export const getBooksData = async () => {
   const books = await db.book.findMany();
