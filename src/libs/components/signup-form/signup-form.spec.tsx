e8f1d876e9203a0bf5f5ec288575936260a6d885
hugorince <hugo.rince@gmail.com>
2024-02-21 21:19:47 +0100

    tests i progress

---
+++ src/libs/components/signup-form/signup-form.spec.tsx
@@ -0,0 +1,51 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { SignUpForm } from ".";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+jest.mock("react-hook-form", () => ({
+  ...jest.requireActual("react-hook-form"),
+  useForm: jest.fn(),
+}));
+
+const mockSubmit = jest.fn();
+
+const mockForm = {
+  register: jest.fn(),
+  handleSubmit: mockSubmit,
+  formState: { s: {} },
+};
+
+const user = userEvent.setup();
+
+describe("SignUpForm component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.requireMock("react-hook-form").useForm.mockReturnValue(mockForm);
+  });
+  it("should set the input values", async () => {
+    render(<SignUpForm />);
+
+    const username = screen.getByLabelText("username");
+    const email = screen.getByLabelText("email");
+    const password = screen.getByLabelText("password");
+    const confirmPassword = screen.getByLabelText("confirm password");
+
+    user.type(username, "username");
+    await waitFor(() => expect(username).toHaveValue("username"));
+
+    user.type(email, "email@mail.com");
+    await waitFor(() => expect(email).toHaveValue("email@mail.com"));
+
+    user.type(password, "password");
+    await waitFor(() => expect(password).toHaveValue("password"));
+
+    user.type(confirmPassword, "password");
+    await waitFor(() => expect(confirmPassword).toHaveValue("password"));
+  });
+});
