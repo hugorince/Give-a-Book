48c5d780eb403aafa77568140dd921a718b23e82
hugorince <hugo.rince@gmail.com>
2024-02-23 16:24:35 +0100

    sign-out redirection + signup link in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import { LoginForm } from "@/libs/components";
 import classes from "./login-page.module.css";
+import Link from "next/link";
 
 export const Login = () => {
   return (
     <main className={classes.pageWrapper}>
       <LoginForm />
+      <Link href="/signup">not an account? sign up !</Link>
     </main>
   );
 };
