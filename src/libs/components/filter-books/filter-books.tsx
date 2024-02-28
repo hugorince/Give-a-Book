acb4def5c68a11987d8db97fdd4ae7a2568764bb
hugorince <hugo.rince@gmail.com>
2024-02-28 14:16:28 +0100

    css on apply filters

--- src/libs/components/filter-books/filter-books.tsx
+++ src/libs/components/filter-books/filter-books.tsx
@@ -2,12 +2,15 @@
 
 import { Button, Checkbox } from "@/libs/ui-components";
 import { filterBooks } from "@/libs/utils";
+import classes from "./filter-books.module.css";
 
 export const FilterBooks = () => {
   return (
-    <form action={filterBooks}>
-      <Checkbox label="Exchange" value="exchange" name="exchange" />
-      <Checkbox label="Give" value="give" name="give" />
+    <form action={filterBooks} className={classes.container}>
+      <div>
+        <Checkbox label="Exchange" value="exchange" name="exchange" />
+        <Checkbox label="Give" value="give" name="give" />
+      </div>
       <Button type="submit">Apply filters</Button>
     </form>
   );
