a1f5db9aa9b6a6a933be8e62f92583b03450e8bf
hugorince <hugo.rince@gmail.com>
2024-03-01 10:21:12 +0100

    login / unlogged in navbar logic

--- src/libs/components/navbar/navbar.tsx
+++ src/libs/components/navbar/navbar.tsx
@@ -3,28 +3,47 @@ import classes from "./navbar.module.css";
 import Link from "next/link";
 import { getInitials } from "@/libs/utils";
 import { SignOutButton } from "./sign-out-button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/auth/auth";
+import { LoginSignUpButton } from "./login-singup-button";
 
 export const Navbar = async () => {
   const initials = await getInitials();
+  const session = await getServerSession(authOptions);
 
   return (
     <header className={classes.header}>
       <nav>
         <ul>
-          <li>
-            <Link href="/profile">
-              <Avatar initials={initials} />
-            </Link>
-          </li>
+          {session && (
+            <li>
+              <Link href="/profile">
+                <Avatar initials={initials} />
+              </Link>
+            </li>
+          )}
           <li>
             <Link href="/books">Books</Link>
           </li>
-          <li>
-            <Link href="/books/post-book">Add book</Link>
-          </li>
-          <li>
-            <SignOutButton />
-          </li>
+          {session ? (
+            <>
+              <li>
+                <Link href="/books/post-book">Add book</Link>
+              </li>
+              <li>
+                <SignOutButton />
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <LoginSignUpButton loginOrSignUp="login" />
+              </li>
+              <li>
+                <LoginSignUpButton loginOrSignUp="signup" />
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
