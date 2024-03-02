19593312f4436609f354cd7f76a6617a219c6d1d
hugorince <hugo.rince@gmail.com>
2024-03-02 11:09:42 +0100

    book card css

--- src/libs/components/book-card/book-card.tsx
+++ src/libs/components/book-card/book-card.tsx
@@ -12,18 +12,16 @@ export const BookCard = ({ data }: { data: BooksData }) => {
       <div className={classes.chip}>
         <Chip label={exchangeOrGive} exchange={data.exchange} />
       </div>
-
-      <Link variant="unstyled" href={`/book/${data.id}`}>
-        <h3>{data.title}</h3>
-      </Link>
-
-      <img src={data.image || ""} alt="" />
       <div className={classes.userLink}>
         <p>offered by </p>
         <Link href={`/user/${data.userId}`} variant="unstyled">
           {data.user}
         </Link>
       </div>
+      <img src={data.image || ""} alt="" />
+      <Link variant="unstyled" href={`/book/${data.id}`}>
+        <h3>{data.title}</h3>
+      </Link>
     </NextLink>
   );
 };
