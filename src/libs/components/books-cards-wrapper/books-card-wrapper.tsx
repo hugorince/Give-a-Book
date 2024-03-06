eb9c53a7df54f1d59cb50d86c304087b5f3414c4
hugorince <hugo.rince@gmail.com>
2024-03-06 20:02:36 +0100

    book card test broken

--- src/libs/components/books-cards-wrapper/books-card-wrapper.tsx
+++ src/libs/components/books-cards-wrapper/books-card-wrapper.tsx
@@ -3,6 +3,8 @@
 import classes from "./books-cards-wrapper.module.css";
 import { getBooksData } from "@/libs/utils";
 import { BookCard } from "..";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/auth/auth";
 
 interface ParamsProps {
   [key: string]: string;
@@ -14,6 +16,8 @@ export const BooksCardWrapper = async ({
   searchParams: ParamsProps;
 }) => {
   const displayBooks = await getBooksData();
+  const user = await getServerSession(authOptions);
+  const userId = user?.user.id || null;
 
   if (searchParams.filter) {
     const params = searchParams.filter.split(",");
@@ -24,14 +28,14 @@ export const BooksCardWrapper = async ({
         {params.length > 1 ? (
           <div className={classes.booksWrapper}>
             {displayBooks.map((book, index) => {
-              return <BookCard data={book} key={index} />;
+              return <BookCard data={book} key={index} userId={userId} />;
             })}
           </div>
         ) : (
           <div className={classes.booksWrapper}>
             {displayBooks.map((book, index) => {
               if (book[singleParam])
-                return <BookCard data={book} key={index} />;
+                return <BookCard data={book} key={index} userId={userId} />;
             })}
           </div>
         )}
@@ -43,7 +47,7 @@ export const BooksCardWrapper = async ({
     <div>
       <div className={classes.booksWrapper}>
         {displayBooks.map((book, index) => {
-          return <BookCard data={book} key={index} />;
+          return <BookCard data={book} key={index} userId={userId} />;
         })}
       </div>
     </div>
