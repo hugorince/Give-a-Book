b4a633db9bcee1aaaaff6fc4b9bc0c31f9ffc2ed
hugorince <hugo.rince@gmail.com>
2024-03-10 19:36:58 +0100

    add like button on user page

--- src/libs/components/books-cards-wrapper/user-books-cards-wrapper/user-books-cards-wrapper.tsx
+++ src/libs/components/books-cards-wrapper/user-books-cards-wrapper/user-books-cards-wrapper.tsx
@@ -3,6 +3,8 @@
 import { getBooksData } from "@/libs/utils";
 import classes from "./user-books-cards-wrapper.module.css";
 import { BookCard } from "../../book-card";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/auth/auth";
 
 interface UserBooksCardsWrapperProps {
   userId: string;
@@ -12,12 +14,28 @@ export const UserBooksCardsWrapper = async ({
   userId,
 }: UserBooksCardsWrapperProps) => {
   const displayBooks = await getBooksData();
+  const user = await getServerSession(authOptions);
+  const connectedUserId = user?.user.id;
+
+  if (connectedUserId) {
+    return (
+      <div className={classes.booksWrapper}>
+        {displayBooks.map((book, index) => {
+          if (book.userId && book.userId.toString() === userId) {
+            return (
+              <BookCard data={book} key={index} userId={connectedUserId} />
+            );
+          }
+        })}
+      </div>
+    );
+  }
 
   return (
     <div className={classes.booksWrapper}>
       {displayBooks.map((book, index) => {
         if (book.userId && book.userId.toString() === userId) {
-          return <BookCard data={book} key={index} />;
+          return <BookCard data={book} key={index} userId={null} />;
         }
       })}
     </div>
