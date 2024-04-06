85d0800fe4d261d90e25a302c411414708a23bfb
hugorince <hugo.rince@gmail.com>
2024-04-06 11:51:31 +0200

    update request book unit test

--- src/libs/components/request-book/request-book.spec.tsx
+++ src/libs/components/request-book/request-book.spec.tsx
@@ -2,22 +2,33 @@ import { render } from "@/libs/utils";
 import { RequestBook } from "./request-book";
 import { mockedBook } from "@/libs/utils";
 import userEvent from "@testing-library/user-event";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import { requestBook } from "@/libs/utils";
+import { useDialog } from "@/libs/ui-components";
+
+const mockOpenDialog = jest.fn();
+
+jest.mock("../../ui-components", () => ({
+  ...jest.requireActual("../../ui-components"),
+  useDialog: jest.fn(),
+}));
 
 const mockRequestBook = jest.fn();
 
 jest.mock("../../utils", () => ({
   ...jest.requireActual("../../utils"),
-  requestBook: mockRequestBook,
+  requestBook: jest.fn(),
 }));
 
-//TODO: Compare and fix regarding StoreLocator lib
-
 describe("RequestBook", () => {
-  it("should trigger the request book function on click", () => {
+  beforeAll(() => {
+    (useDialog as jest.Mock).mockReturnValue({ openDialog: mockOpenDialog });
+    (requestBook as jest.Mock).mockImplementation(mockRequestBook);
+  });
+  it("should trigger open the dialog on click", async () => {
     render(<RequestBook book={mockedBook} />);
 
     userEvent.click(screen.getByRole("button", { name: "Request book" }));
-    expect(mockRequestBook).toHaveBeenCalled();
+    await waitFor(() => expect(mockOpenDialog).toHaveBeenCalled());
   });
 });
