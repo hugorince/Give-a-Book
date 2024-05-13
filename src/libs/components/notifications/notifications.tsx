d768f2245b30b7ebc4f2e38092414909856f479d
hugorince <hugo.rince@gmail.com>
2024-05-13 19:12:17 +0200

    notification number

--- src/libs/components/notifications/notifications.tsx
+++ src/libs/components/notifications/notifications.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/libs/ui-components";
 import { useState } from "react";
 import { DropdownNotifications } from "./dropdown-notifications";
+import { IoMdNotificationsOutline } from "react-icons/io";
 import classes from "./notifications.module.css";
 
 export interface NotificationProps {
@@ -22,10 +23,22 @@ export const Notifications = ({ notifications }: NotificationsProps) => {
     setIsOpen((prev) => !prev);
   };
 
+  const notificationNonReadNumber = notifications.filter(
+    (notification) => !notification.isRead,
+  ).length;
+
   return (
     <div className={classes.notificationsContainer}>
-      <Button onClick={handleOnClick} size="s">
-        notifications
+      <Button
+        variant="unstyled"
+        size="s"
+        onClick={handleOnClick}
+        className={classes.notificationsButton}
+      >
+        <IoMdNotificationsOutline size={24} />
+        {notificationNonReadNumber > 0 && (
+          <div>{notificationNonReadNumber}</div>
+        )}
       </Button>
       {isOpen && (
         <DropdownNotifications
