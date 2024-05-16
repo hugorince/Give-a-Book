dd26e54c97d4eaef0aaa7eb8ca7794c3c0459d1e
hugorince <hugo.rince@gmail.com>
2024-05-16 15:55:56 +0200

    write message testing in progress

---
+++ src/libs/components/chat/write-message/write-message.spec.tsx
@@ -0,0 +1,36 @@
+import { render } from "@/libs/test-utils";
+import { WriteMessage } from "./write-message";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { sendMessage } from "@/libs/database";
+
+const mockSendMessage = jest.fn();
+
+jest.mock("../../../database", () => ({
+  sendMessage: jest.fn(),
+}));
+
+describe("WriteMessage", () => {
+  beforeAll(() => {
+    (sendMessage as jest.Mock).mockImplementation(mockSendMessage);
+  });
+  it("should render the text box", () => {
+    render(<WriteMessage chatId={1} />);
+
+    expect(screen.getByLabelText("write message")).toBeVisible();
+    expect(screen.getByRole("button", { name: "Send message" })).toBeVisible();
+  });
+
+  it("should send the message on submit", () => {
+    render(<WriteMessage chatId={1} />);
+
+    const textarea = screen.getByLabelText("write message");
+    const submitButton = screen.getByRole("button", { name: "Send message" });
+
+    userEvent.type(textarea, "message");
+
+    userEvent.click(submitButton);
+
+    expect(mockSendMessage).toHaveBeenCalledWith("message", 1);
+  });
+});
