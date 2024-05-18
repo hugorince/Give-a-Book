29c02debb4097e1c5a3fa1dac3b918140f4e6aa3
hugorince <hugo.rince@gmail.com>
2024-05-18 11:27:21 +0200

    test utils

--- src/libs/components/post-book-form/description-input/description-input.spec.tsx
+++ src/libs/components/post-book-form/description-input/description-input.spec.tsx
@@ -1,33 +1,25 @@
-import type { ReactNode } from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import { render, RHFWrapper, mockOnSubmit } from "@/libs/test-utils";
 import { DescriptionInput } from ".";
-import { FormProvider, useForm } from "react-hook-form";
 import userEvent from "@testing-library/user-event";
-
-const mockOnSubmit = jest.fn();
-
-const Wrapper = ({ children }: { children: ReactNode }) => {
-  const form = useForm();
-
-  return (
-    <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(mockOnSubmit)}>
-        {children}
-        <button type="submit">submit</button>
-      </form>
-    </FormProvider>
-  );
-};
 
 describe("DescriptionInput", () => {
   it("should correctly render the component", () => {
-    render(<DescriptionInput />, { wrapper: Wrapper });
+    render(
+      <RHFWrapper>
+        <DescriptionInput />
+      </RHFWrapper>,
+    );
 
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
   it("should set the value on submit", async () => {
-    render(<DescriptionInput />, { wrapper: Wrapper });
+    render(
+      <RHFWrapper>
+        <DescriptionInput />
+      </RHFWrapper>,
+    );
 
     const user = userEvent.setup();
 
