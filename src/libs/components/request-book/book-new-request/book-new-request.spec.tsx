7805435bee8d8072d09e7af36c9a8f9ccbb15da3
hugorince <hugo.rince@gmail.com>
2024-06-14 10:47:10 +0200

    testing update

--- src/libs/components/request-book/book-new-request/book-new-request.spec.tsx
+++ src/libs/components/request-book/book-new-request/book-new-request.spec.tsx
@@ -1,10 +1,10 @@
-import { mockedBookedBook, render } from "@/libs/test-utils";
+import { mockedBookPage, render } from "@/libs/test-utils";
 import { BookNewRequest } from "./book-new-request";
 import { screen } from "@testing-library/react";
 
 describe("BookNewRequest", () => {
   it("should show the request button", () => {
-    render(<BookNewRequest book={mockedBookedBook} />);
+    render(<BookNewRequest book={mockedBookPage} />);
 
     expect(screen.getByRole("button", { name: "Request book" })).toBeVisible();
   });
