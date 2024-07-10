461b78e5978f8a60a0b4d6871003f69582a65993
hugorince <hugo.rince@gmail.com>
2024-07-10 20:33:27 +0200

    chat upgrade part 2

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -7,14 +7,20 @@ import classes from "./chat.module.css";
 
 interface ChatProps {
   messages: MessageType[];
+  title: string;
+  userName: string | null | undefined;
 }
 
-export const Chat = ({ messages }: ChatProps) => {
+export const Chat = ({ messages, title, userName }: ChatProps) => {
   const chatId = messages[0].chatId;
   const reverseMessages = messages.reverse();
 
   return (
     <div className={classes.chatContainer}>
+      <div className={classes.contactContainer}>
+        <p className={classes.userName}>{userName}</p>
+        <p className={classes.bookTitle}>{title}</p>
+      </div>
       <div className={classes.messagesContainer}>
         {reverseMessages.map((message, key) => (
           <Message key={key} message={message} />
