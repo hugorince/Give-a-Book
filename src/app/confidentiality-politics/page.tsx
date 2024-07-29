e6e738064c27249daf632d98a15638c5f0b08502
hugorince <hugo.rince@gmail.com>
2024-07-29 18:46:24 +0200

    confidentitality agreement

---
+++ src/app/confidentiality-politics/page.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "@/layout";
+
+const ConfidentialityPolitics = () => {
+  return (
+    <MainLayout>
+      <h1>Confidentiality politics</h1>
+    </MainLayout>
+  );
+};
+
+export default ConfidentialityPolitics;
--- src/ui-kit/components/checkbox/checkbox.tsx
+++ src/ui-kit/components/checkbox/checkbox.tsx
@@ -1,18 +1,25 @@
-import { type ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentPropsWithRef } from "react";
 
-interface CheckboxProps extends ComponentPropsWithoutRef<"input"> {
+interface CheckboxProps extends ComponentPropsWithRef<"input"> {
   label: string;
+  link?: {
+    label: string;
+    href: string;
+  };
 }
 
-export const Checkbox = ({ label, ...props }: CheckboxProps) => {
-  return (
-    <div className="checkbox-container">
-      <label htmlFor={label}>
-        <input type="checkbox" id={label} {...props} />
-        {label}
-      </label>
-    </div>
-  );
-};
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, link, ...props }, ref) => {
+    return (
+      <div className="checkbox-container">
+        <label htmlFor={label}>
+          <input type="checkbox" id={label} {...props} ref={ref} />
+          {label}
+        </label>
+        {link && <a href={link.href}>{link.label}</a>}
+      </div>
+    );
+  },
+);
 
 Checkbox.displayName = "Checkbox";
