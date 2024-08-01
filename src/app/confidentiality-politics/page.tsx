b0d31e9d5d953c1bf2943da245fa7fb6dd905253
hugorince <hugo.rince@gmail.com>
2024-08-01 21:05:35 +0200

    profile page update + confidentiality page

--- src/app/confidentiality-politics/page.tsx
+++ src/app/confidentiality-politics/page.tsx
@@ -4,6 +4,10 @@ const ConfidentialityPolitics = () => {
   return (
     <MainLayout>
       <h1>Confidentiality politics</h1>
+      <p>
+        All my personal data can be deleted by requesting the team. When I
+        delete my account, no personal data will be kept.
+      </p>
     </MainLayout>
   );
 };
