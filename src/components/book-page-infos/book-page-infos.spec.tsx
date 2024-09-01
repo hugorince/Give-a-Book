667c35e8e088da11c437689350390981527a368f
hugorince <hugo.rince@gmail.com>
2024-09-01 19:30:36 +0200

    add distance and owner link to book page

--- src/components/book-page-infos/book-page-infos.spec.tsx
+++ src/components/book-page-infos/book-page-infos.spec.tsx
@@ -4,8 +4,8 @@ import { mockedBookPage } from "@/test-utils";
 import { render } from "@/test-utils";
 
 describe("BookPageInfos", () => {
-  it("should map the data to display the correct infos", () => {
-    render(<BookPageInfos book={mockedBookPage} connectedUserId={1} />);
+  it("should map the data to display the correct infos", async () => {
+    render(await BookPageInfos({ book: mockedBookPage, connectedUserId: 1 }));
 
     expect(screen.getByText("title")).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveAttribute("src", "image");
--- src/components/book-page-infos/book-page-infos.tsx
+++ src/components/book-page-infos/book-page-infos.tsx
@@ -1,25 +1,41 @@
 "use server";
 
 import type { BookPageData } from "@/types";
-import { postedOn } from "@/utils";
-import { Chip } from "@/ui-kit";
+import { calculateDistance, postedOn } from "@/utils";
+import { Chip, Link } from "@/ui-kit";
 import { LikeButton } from "../like-button";
 import classes from "./book-page-infos.module.css";
+import { getUserInfo } from "@/actions";
 
 interface BookPageInfosProps {
   book: BookPageData;
   connectedUserId: number | undefined;
 }
 
-export const BookPageInfos = ({
+export const BookPageInfos = async ({
   book,
   connectedUserId,
 }: BookPageInfosProps) => {
   const requestTypeLabel = book.exchange ? "Exchange" : "Give";
   const posted = postedOn(book.createdAt);
-
   const isLiked =
     connectedUserId && book.likes.includes(connectedUserId) ? true : false;
+
+  const getDistance = async () => {
+    if (!connectedUserId) return null;
+
+    const connectedInfos = await getUserInfo(connectedUserId);
+    const ownerInfos = await getUserInfo(book.userId);
+
+    if (!connectedInfos || !ownerInfos) return null;
+
+    return calculateDistance(
+      connectedInfos.gpsCoordinates,
+      ownerInfos.gpsCoordinates,
+    );
+  };
+
+  const distance = await getDistance();
 
   return (
     <div className={classes.wrapper}>
@@ -28,6 +44,7 @@ export const BookPageInfos = ({
       </div>
       <div className={classes.bookInfos}>
         <div className={classes.chipDate}>
+          {distance !== null && <p>{Math.floor(distance)} km from you</p>}
           <p>{posted}</p>
           {book.requested && <Chip label="requested" variant="requested" />}
           <Chip label={requestTypeLabel} exchange={book.exchange} />
@@ -38,6 +55,9 @@ export const BookPageInfos = ({
             isLoggedIn={connectedUserId !== undefined}
           />
         </div>
+        <Link href={`/user/${book.userId}`} variant="unstyled">
+          proposed by {book.username}
+        </Link>
         <h1>{book.title}</h1>
         <h2>{book.author}</h2>
         <p>{book.description}</p>
