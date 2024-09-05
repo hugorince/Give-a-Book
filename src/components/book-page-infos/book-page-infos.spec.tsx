360ca9352672c27afdcd3df2a825a6a84684bb4e
hugorince <hugo.rince@gmail.com>
2024-09-05 09:00:25 +0200

    fix book page infos for own book

--- src/components/book-page-infos/book-page-infos.spec.tsx
+++ src/components/book-page-infos/book-page-infos.spec.tsx
@@ -2,6 +2,7 @@ import { BookPageInfos } from ".";
 import { screen } from "@testing-library/react";
 import { mockedBookPage } from "@/test-utils";
 import { render } from "@/test-utils";
+import { getUserInfo } from "@/actions";
 
 jest.mock("../../actions", () => ({
   getUserInfo: jest.fn(),
@@ -14,5 +15,14 @@ describe("BookPageInfos", () => {
     expect(screen.getByText("title")).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveAttribute("src", "image");
     expect(screen.getByText("Exchange")).toBeInTheDocument();
+    expect(screen.getByTestId("liked-button")).toBeInTheDocument();
+  });
+
+  it("should remove like button and link to the user when is own book", async () => {
+    (getUserInfo as jest.Mock).mockReturnValue({ id: 2 });
+    render(await BookPageInfos({ book: mockedBookPage, connectedUserId: 2 }));
+
+    expect(screen.queryByTestId("liked-button")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("not-liked-button")).not.toBeInTheDocument();
   });
 });
--- src/components/book-page-infos/book-page-infos.tsx
+++ src/components/book-page-infos/book-page-infos.tsx
@@ -1,15 +1,15 @@
 "use server";
 
 import type { BookPageData } from "@/types";
+import { getUserInfo } from "@/actions";
 import { calculateDistance, postedOn } from "@/utils";
 import { Chip, Link } from "@/ui-kit";
 import { LikeButton } from "../like-button";
 import classes from "./book-page-infos.module.css";
-import { getUserInfo } from "@/actions";
 
 interface BookPageInfosProps {
   book: BookPageData;
-  connectedUserId: number | undefined;
+  connectedUserId?: number;
 }
 
 export const BookPageInfos = async ({
@@ -17,20 +17,22 @@ export const BookPageInfos = async ({
   connectedUserId,
 }: BookPageInfosProps) => {
   const requestTypeLabel = book.exchange ? "Exchange" : "Give";
-  const posted = postedOn(book.createdAt);
+  const postedOnLabel = postedOn(book.createdAt);
   const isLiked =
     connectedUserId && book.likes.includes(connectedUserId) ? true : false;
 
+  const connectedUserInfos =
+    connectedUserId && (await getUserInfo(connectedUserId));
+  const ownerInfos = await getUserInfo(book.userId);
+  const isOwnBook =
+    connectedUserInfos && connectedUserInfos?.id === ownerInfos?.id;
+
   const getDistance = async () => {
-    if (!connectedUserId) return null;
-
-    const connectedInfos = await getUserInfo(connectedUserId);
-    const ownerInfos = await getUserInfo(book.userId);
-
-    if (!connectedInfos || !ownerInfos) return null;
+    if (!connectedUserId || !connectedUserInfos || !ownerInfos || isOwnBook)
+      return null;
 
     return calculateDistance(
-      connectedInfos.gpsCoordinates,
+      connectedUserInfos.gpsCoordinates,
       ownerInfos.gpsCoordinates,
     );
   };
@@ -44,20 +46,26 @@ export const BookPageInfos = async ({
       </div>
       <div className={classes.bookInfos}>
         <div className={classes.chipDate}>
-          {distance !== null && <p>{Math.floor(distance)} km from you</p>}
-          <p>{posted}</p>
+          {distance !== null && !isOwnBook && (
+            <p>{Math.floor(distance)} km from you</p>
+          )}
+          <p>{postedOnLabel}</p>
           {book.requested && <Chip label="requested" variant="requested" />}
           <Chip label={requestTypeLabel} exchange={book.exchange} />
-          <LikeButton
-            isLiked={isLiked}
-            bookId={book.id}
-            likesNumber={book.likes.length}
-            isLoggedIn={connectedUserId !== undefined}
-          />
+          {!isOwnBook && (
+            <LikeButton
+              isLiked={isLiked}
+              bookId={book.id}
+              likesNumber={book.likes.length}
+              isLoggedIn={connectedUserId !== undefined}
+            />
+          )}
         </div>
-        <Link href={`/user/${book.userId}`} variant="unstyled">
-          proposed by {book.username}
-        </Link>
+        {!isOwnBook && (
+          <Link href={`/user/${book.userId}`} variant="unstyled">
+            proposed by {book.username}
+          </Link>
+        )}
         <h1>{book.title}</h1>
         <h2>{book.author}</h2>
         <p>{book.description}</p>
